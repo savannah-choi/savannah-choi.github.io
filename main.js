39a6f241d07532832bfd1c674b38d38b204c2830
Savannah Choi <senotia23@gmail.com>
2022-05-11 01:43:53 +0900

    modify max percentage

--- main.js
+++ main.js
@@ -20,13 +20,14 @@ function getDiceForm () {
     var res2 = Math.ceil(30 + (multinum * favor2));
     var res3 = Math.ceil(30 + (multinum * favor3));
 
-    var maxnum = (Math.ceil((res1 + res2 + res3) * 0.1) * 10) + 10;
+    var sum = res1+res2+res3;
+    var maxnum = sum + Math.ceil(sum * 0.1) + 1;
 
     var str = "";
     str += ("1~" + res1 + " : " + name1 + "(" + (res1) + ")" + "<br/>");
     str += ((res1+1) + "~" + (res1+res2) + " : " + name2 + "(" + (res2) + ")" + "<br/>");
     str += ((res1+res2+1) + "~" + (res1+res2+res3) + " : " + name3 + "(" + (res3) + ")" + "<br/>");
-    str += ((res1+res2+res3+1) + "~" + maxnum + " : +1" + "(" + (maxnum-res1-res2-res3) + ")");
+    str += ((res1+res2+res3+1) + "~" + maxnum + " : +1" + "(" + (maxnum-sum) + ")");
     str += "<br/><br/>";
     str += ("\`주사위 " + maxnum + "\`<br/>\`highlight\`");
     
